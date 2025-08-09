1ad83ee6fc80829ed90154da30842d956100f290
codecroo <parthdelvadiya1475@gmail.com>
2025-08-09 12:06:57 +0530

    .

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 const token = localStorage.getItem('access_token');
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/',
+  baseURL: 'http://localhost:8000',
   headers: {
-    Authorization: token ? `Bearer ${token}` : '',
+    Authorization: `Bearer ${token}`,
   },
 });
 
